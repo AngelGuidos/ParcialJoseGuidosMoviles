62fabf80ff7926f89950b9ab5aed9d05fb4e89bf
AngelGuidos <00137321@uca.edu.sv>
2023-05-22 13:44:59 -0600

    Capa modelo

---
+++ ParcialJoseGuidos/app/src/main/java/com/galacticstudio/parcialjoseguidos/data/DummyData.kt
@@ -0,0 +1,10 @@
+package com.galacticstudio.parcialjoseguidos.data
+
+import com.galacticstudio.parcialjoseguidos.data.models.LakeModel
+
+val name = "XLake"
+val surface = "999999mt"
+
+val lakes = mutableListOf(
+    LakeModel(name, surface)
+)
---
+++ ParcialJoseGuidos/app/src/main/java/com/galacticstudio/parcialjoseguidos/data/models/LakeModel.kt
@@ -0,0 +1,6 @@
+package com.galacticstudio.parcialjoseguidos.data.models
+
+data class LakeModel(
+    val name: String,
+    val surface: String
+)
